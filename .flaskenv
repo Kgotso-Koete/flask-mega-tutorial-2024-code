90d0f957accd4b770e63b8a4eda1bf1b06c857cd
Kgotso-Koete <kgkoete@gmail.com>
2025-05-29 21:00:03 +0200

    chapter 10

--- .flaskenv
+++ .flaskenv
@@ -1,4 +1,1 @@
 FLASK_APP=microblog.py
-export MAIL_SERVER=localhost
-export MAIL_PORT=8025
-FLASK_DEBUG=0
--- app/models.py
+++ app/models.py
@@ -7,6 +7,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 from hashlib import md5
+from time import time
+import jwt
+from app import app
+
 
 followers = sa.Table(
     'followers',
@@ -42,6 +46,9 @@ class User(UserMixin, db.Model):
         secondaryjoin=(followers.c.follower_id == id),
         back_populates='following')
     
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+        
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -93,8 +100,21 @@ class User(UserMixin, db.Model):
             .order_by(Post.timestamp.desc())
         )
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
+
+    
 
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
